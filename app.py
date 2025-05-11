1f74f7724d0042a1e924df5032d20c1750c5be6a
Mercy <oajagemo@terpmail.umd.edu>
2025-05-11 18:35:20 -0400

    second commit

--- app.py
+++ app.py
@@ -2,14 +2,20 @@ import random
 
 suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
 deck = []
+player_hand = []
+computer_hand = []
 
 def create_deck():
     deck = []
     for suit in suits:
-        for number in range(1, 13):  # 1 through 12, no Kingggsss
+        for number in range(1, 13):
             deck.append((number, suit))
     random.shuffle(deck)
     return deck
+
+def deal_cards(deck, hand, count=8):
+    for _ in range(count):
+        hand.append(deck.pop())
 
 def show_card(card):
     return str(card[0]) + " of " + card[1]
@@ -19,8 +25,44 @@ def show_hand(hand):
         card = hand[i]
         print(str(i + 1) + ": " + str(card[0]) + " of " + card[1])
 
-if __name__ == "__main__":
+def play_card(hand, lead_suit=None):
+    for card in hand:
+        if lead_suit and card[1] == lead_suit:
+            hand.remove(card)
+            return card
+    return hand.pop(0)
+
+def determine_winner(card1, card2, lead_suit):
+    if card2[1] != lead_suit:
+        return "player"
+    return "player" if card1[0] > card2[0] else "computer"
+
+def play_game():
+    global deck
     deck = create_deck()
-    print("Shuffled deck:")
-    for card in deck:
-        print(show_card(card))
+    deal_cards(deck, player_hand)
+    deal_cards(deck, computer_hand)
+    leader = "player"
+
+    while player_hand and computer_hand:
+        if leader == "player":
+            lead = play_card(player_hand)
+            follow = play_card(computer_hand, lead[1])
+        else:
+            lead = play_card(computer_hand)
+            follow = play_card(player_hand, lead[1])
+
+        print("Player played:", show_card(lead))
+        print("Computer played:", show_card(follow))
+
+        winner = determine_winner(lead, follow, lead[1])
+        if winner == "player":
+            leader = "player"
+            print("Player wins the round!")
+        else:
+            leader = "computer"
+            print("Computer wins the round!")
+
+if __name__ == "__main__":
+    play_game()
+
