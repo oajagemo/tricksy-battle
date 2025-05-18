177df17cd34cd872341a2bac5db0562bf0a9133d
Mercy <oajagemo@terpmail.umd.edu>
2025-05-18 18:43:04 -0400

    Third commit

--- app.py
+++ app.py
@@ -1,44 +1,55 @@
 import random
 
+
 suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
 deck = []
 player_hand = []
 computer_hand = []
+player_points = 0
+computer_points = 0
 
+# Create deck
 def create_deck():
-    deck = []
+    new_deck = []
     for suit in suits:
-        for number in range(1, 13):
-            deck.append((number, suit))
-    random.shuffle(deck)
-    return deck
+        for number in range(1, 13):  # 1-12, no King
+            new_deck.append((number, suit))
+    random.shuffle(new_deck)
+    return new_deck
+
 
 def deal_cards(deck, hand, count=8):
-    for _ in range(count):
+    for i in range(count):
         hand.append(deck.pop())
+
 
 def show_card(card):
     return str(card[0]) + " of " + card[1]
 
+# Show all cards 
 def show_hand(hand):
     for i in range(len(hand)):
         card = hand[i]
-        print(str(i + 1) + ": " + str(card[0]) + " of " + card[1])
+        print(str(i + 1) + ": " + show_card(card))
 
+# Let the player choose a card
 def player_choose_card(hand, lead_suit=None):
     while True:
         print("\nYour hand:")
         show_hand(hand)
+
         try:
             choice = int(input("Choose a card to play: ")) - 1
             chosen = hand[choice]
 
-            if lead_suit:
-                has_suit = False
+            
+            if lead_suit is not None:
+                has_same_suit = False
                 for card in hand:
                     if card[1] == lead_suit:
-                        has_suit = True
-                if chosen[1] != lead_suit and has_suit:
+                        has_same_suit = True
+
+                if has_same_suit and chosen[1] != lead_suit:
                     print("You must follow the lead suit!")
                     continue
 
@@ -47,44 +58,71 @@ def player_choose_card(hand, lead_suit=None):
         except:
             print("Invalid choice. Try again.")
 
-def play_card(hand, lead_suit=None):
+
+def computer_play_card(hand, lead_suit=None):
     for card in hand:
-        if lead_suit and card[1] == lead_suit:
+        if lead_suit is not None and card[1] == lead_suit:
             hand.remove(card)
             return card
     return hand.pop(0)
 
+
 def determine_winner(card1, card2, lead_suit):
     if card2[1] != lead_suit:
         return "player"
-    return "player" if card1[0] > card2[0] else "computer"
+    elif card1[0] > card2[0]:
+        return "player"
+    else:
+        return "computer"
+
 
 def play_game():
-    global deck
+    global deck, player_points, computer_points
+
     deck = create_deck()
     deal_cards(deck, player_hand)
     deal_cards(deck, computer_hand)
     leader = "player"
 
-    while player_hand and computer_hand:
+    print("=== Welcome to Tricksy Battle ===")
+
+    while len(player_hand) > 0 and len(computer_hand) > 0:
         print("\n--- New Round ---")
+
         if leader == "player":
-            lead = player_choose_card(player_hand)
-            follow = play_card(computer_hand, lead[1])
+            lead_card = player_choose_card(player_hand)
+            follow_card = computer_play_card(computer_hand, lead_card[1])
+            print("You played:", show_card(lead_card))
+            print("Computer played:", show_card(follow_card))
         else:
-            lead = play_card(computer_hand)
-            follow = player_choose_card(player_hand, lead[1])
+            lead_card = computer_play_card(computer_hand)
+            follow_card = player_choose_card(player_hand, lead_card[1])
+            print("Computer played:", show_card(lead_card))
+            print("You played:", show_card(follow_card))
 
-        print("Player played:", show_card(lead))
-        print("Computer played:", show_card(follow))
+        winner = determine_winner(lead_card, follow_card, lead_card[1])
 
-        winner = determine_winner(lead, follow, lead[1])
         if winner == "player":
+            player_points += 1
             leader = "player"
             print("You win the round!")
         else:
+            computer_points += 1
             leader = "computer"
             print("Computer wins the round!")
 
+        print("Score:")
+        print("You:", player_points)
+        print("Computer:", computer_points)
+
+    print("\n=== Game Over ===")
+    if player_points > computer_points:
+        print("You win the game!")
+    elif computer_points > player_points:
+        print("Computer wins the game!")
+    else:
+        print("It's a tie!")
+
+# Run the game
 if __name__ == "__main__":
     play_game()
